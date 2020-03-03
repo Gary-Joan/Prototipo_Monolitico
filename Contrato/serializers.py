505c1c20d16476483803fee83f9da426e3ecc06f
@dan192596 <dan192596@gmail.com>
2020-03-02 22:41:27 -0600

    Se crearon los serializadores

---
+++ Contrato/serializers.py
@@ -0,0 +1,27 @@
+from Contrato.models import Contrato, Contrato_Menu
+from Ingrediente.serializers import IngredienteSerializer
+from Menu.serializers import MenuSerializer
+from Montaje.serializers import MontajeSerializer
+from Restaurante.serializers import RestauranteSerializer
+from Salon.serializers import SalonSerializer
+from User.serializers import UserSerializer
+from rest_framework import serializers
+
+class ContratoSerializer(serializers.ModelSerializer):
+    Cliente = UserSerializer(read_only=True)
+    Gerente = UserSerializer(read_only=True)
+    Menu = MenuSerializer(read_only=True)
+    Montaje = MontajeSerializer(read_only=True)
+    Restaurante = RestauranteSerializer(read_only=True)
+    Salon = SalonSerializer(read_only=True)
+
+    class Meta:
+        model = Contrato
+        fields = ('id', 'Cliente', 'Gerente', 'Menu', 'Montaje', 'Restaurante', 'Salon')
+
+class ContratoMenuSerializer(serializers.ModelSerializer):
+    Menu = MenuSerializer(read_only=True)
+
+    class Meta:
+        model = Contrato_Menu
+        fields = ('id', 'Menu')
